245a0242e20b8ca7625776cc8059f6d7d760341f
Jilmer Padua <jiis.padua.up@phinamed.com>
2023-11-05 07:32:59 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+const app = express()
+
+require("dotenv").config();
+
+app.use(cors())
+app.use(bodyParser.json())
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    // Start the server after MongoDB connection is established
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+  });
